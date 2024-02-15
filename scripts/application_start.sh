f3893bc4bbe49eb955a98a2a9abf47e520a51cac
Hoàng Trọng Tấn <88757683+hoang-trong-tan@users.noreply.github.com>
2024-02-16 00:16:34 +0700

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/DATN-K26/BACKEND/
+echo 'run application_start.sh: ' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'npm start nodejs-app' >> /home/ec2-user/DATN-K26/BACKEND/
-npm start >> /home/ec2-user/DATN-K26/BACKEND/
+echo 'npm start nodejs-app' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
+npm start >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
