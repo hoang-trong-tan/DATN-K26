e6456fd082e4e1c6ac024c6cdd36d0c4041b762f
hoang-trong-tan <esttan2018@gmail.com>
2024-02-16 00:35:47 +0700

    d2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,6 @@
 
 echo 'run application_start.sh: ' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'npm start' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
-cd /home/ec2-user/DATN-K26/BACKEND
+echo 'cd /home/ec2-user/DATN-K26/BACKEND' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
+cd /home/ec2-user/DATN-K26/BACKEND >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
 npm start >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
