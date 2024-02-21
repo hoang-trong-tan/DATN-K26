de84571bc738e0d307d741b379255c92f6ddcb5d
hoang-trong-tan <esttan2018@gmail.com>
2024-02-21 15:28:45 +0700

    up

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/DATN-K26/deploy.log
 
-echo 'cd /home/ec2-user/DATN-K26/BACKEND' >> /home/ec2-user/DATN-K26/deploy.log
-cd /home/ec2-user/DATN-K26/BACKEND >> /home/ec2-user/DATN-K26/deploy.log
+echo 'cd /home/ec2-user/DATN-K26 >> /home/ec2-user/deploy.log
+cd /home/ec2-user/DATN-K26 >> /home/ec2-user/deploy.log
 
 echo 'npm install' >> /home/ec2-user/DATN-K26/deploy.log 
 npm install >> /home/ec2-user/DATN-K26/deploy.log
