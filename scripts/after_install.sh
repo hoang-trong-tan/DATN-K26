7952d303dd4d91dab2f7795c1819af5d84103de0
hoang-trong-tan <esttan2018@gmail.com>
2024-02-22 17:20:37 +0700

    fix

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
+echo 'run after_install.sh: ' >> /home/ec2-user/deploy.log
 
-echo 'cd /home/ec2-user/DATN-K26/BACKEND' >> /home/ec2-user/BACKEND/deploy.log
+echo 'cd /home/ec2-user/DATN-K26/BACKEND' >> /home/ec2-user/deploy.log
 cd /home/ec2-user/DATN-K26/BACKEND >> /home/ec2-user/deploy.log
 
-echo 'npm install' >> /home/ec2-user/DATN-K26/BACKEND/deploy.log 
-npm install >> /home/ec2-user/DATN-K26/BACKEND/deploy.log
+echo 'npm install' >> /home/ec2-user/deploy.log 
+npm install >> /home/ec2-user/deploy.log
